4cd16d8e5fde31b602968851d14e17f4d8d9bef4
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2023-03-11 03:22:42 -0800

    create_static_lib.sh

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,14 +1,7 @@
 #include "main.h"
-/**
-* main - Entry point
-*
-* Description - print to the output
-* Return: Always '0' (success)
-*/
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best
-hacker\"\n\t- Kevin Mitnick");
-
-	return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
