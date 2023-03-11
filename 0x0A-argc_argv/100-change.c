05992630372837261c2ae5b473f133d044abc9f8
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2023-03-10 19:24:45 -0800

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - two parameters taken
+ *
+ * Description - to count number of array pass
+ *
+ * Return: Always '0' (success)
+ */
+int main(int argc, char *argv[])
+{
+	int cents;
+	int num_coins;
+
+	if (argc != 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+	printf("0\n");
+	return (0);
+	}
+
+	num_coins = 0;
+
+	num_coins += cents / 25; /*number of quarters*/
+	cents %= 25;
+
+	num_coins += cents / 10; /*number of dimes*/
+	cents %= 10;
+
+	num_coins += cents / 5; /*number of nickels*/
+	cents %= 5;
+
+	num_coins += cents / 2; /*number of twopence*/
+	cents %= 2;
+
+	num_coins += cents; /*number of pennies*/
+
+	printf("%d\n", num_coins);
+
+	return (0);
+}
