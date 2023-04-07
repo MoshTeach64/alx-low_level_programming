36cec68d6468614a42e6e5b14488b4b0c3c6788c
MoshTeach64 <odutolaezikiel1985@gmail.com>
2023-04-07 11:11:19 -0700

    my new discovery

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,26 @@
 
 void print_rev(char *s)
 {
-	int len = 0;
-	int counter;
+	/*int len = 0;*/
+	/*int counter;*/
 
-	len = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
+	/*len = 0;*/
+	/*while (s[len] != '\0')*/
+	/*{*/
+		/*len++;*/
+	/*}*/
 
-	for (counter = (len - 1); counter >= 0; counter--)
+	/*for (counter = (len - 1); counter >= 0; counter--)*/
+	/*{*/
+		/*putchar(s[counter]);*/
+	/*}*/
+	/*putchar('\n');*/
+
+	int  i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
 	{
-		putchar(s[counter]);
+		_putchar(s[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
