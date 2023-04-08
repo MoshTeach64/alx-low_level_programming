5959a2ec8c29ce3df7b47934f82452f980c122b4
MoshTeach64 <odutolaezikiel1985@gmail.com>
2023-04-07 22:08:16 -0700

    my Better way of writing it

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,23 +9,20 @@
 
 void print_square(int size)
 {
-	int inc1, inc2;
+	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (inc1 = 0; inc1 < size; inc1++)
-		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
-			{
-				putchar('#');
-			}
+		_putchar('\n');
 
-			putchar('#');
-			putchar('\n');
-		}
+		return;
 	}
-	else
+	for (i = 0; i < size; i++)
 	{
-		putchar('\n');
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
 	}
 }
