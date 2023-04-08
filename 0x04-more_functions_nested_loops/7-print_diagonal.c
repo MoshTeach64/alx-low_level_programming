d8451f3992625555c32142277b15c69beb006a9e
MoshTeach64 <odutolaezikiel1985@gmail.com>
2023-04-07 21:26:17 -0700

    my better way of doing it

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,26 +8,22 @@
 
 void print_diagonal(int n)
 {
-	int len, space;
+	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (len = 0; len < n; len++)
-		{
-			for (space = 0; space < len; space++)
-			{
-				putchar(' ');
-			}
+		_putchar('\n');
 
-			putchar('\\');
-
-			if (len == (n - 1))
-			{
-				continue;
-			}
-			putchar('\n');
-		}
+		return;
 	}
-	putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			/* @j is to print the space*/
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 }
-
