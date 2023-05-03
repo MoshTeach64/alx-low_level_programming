fc0a94438bec242f2f0fb07e0c96c44929bce2ea
Moshood Odutola Ezekiel <odutolaezikiel1985@gmail.com>
2023-05-03 07:24:32 -0700

    My personal code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,11 +15,9 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 		{
 			d->name = "(nil)";
-			
 			printf("Name: %s\n", d->name);
 		}
 		else
-			printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
-	return;
 }
