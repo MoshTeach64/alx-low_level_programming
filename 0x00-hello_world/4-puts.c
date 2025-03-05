f712834e8f2c4de14c07746ac4903042744ed329
MosTeach64 <odutolamoshood1985@gmail.com>
2025-03-04 18:45:04 -0800

    C program using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message using the puts functions.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
